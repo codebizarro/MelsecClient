0884d74908c2af1dce01f1e0c5f2b9e627b9d8a0
Rudenko.A <Rudenko.A.@localhost>
2015-11-26 09:40:02 +0200

    ? ?????????? ?????? ????????? ???????? ?? ????? ????, ????? 2 ? 4 ?????

--- Globals.cs
+++ Globals.cs
@@ -7,5 +7,6 @@
         public const string NO_DATA_READ = "No data to read";
         public const string ODD_SIZE_ARRAY = "Odd size array values not supported";
         public const string LESS_ZERO_TIMEOUT = "Timeout must be greater than zero";
+        public const string WRONG_TYPE_SIZE = "Size of <T> must be between 2 and 4 byte";
     }
 }
