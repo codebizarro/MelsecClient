74bc6df04a557972dcd86a30fc11076ca281d940
Rudenko.Artem <Rudenko.Artem@localhost>
2018-06-03 17:37:41 +0300

    Refactoring

--- MelsecClient/Melsec3EProtocol.cs
+++ MelsecClient/Melsec3EProtocol.cs
@@ -4,14 +4,13 @@ namespace System.Net.Melsec
 {
     public sealed class Melsec3EProtocol : MelsecEthProtocol
     {
-        private const int ERROR_CODE_POSITION = 9;
-        private const int MIN_RESPONSE_LENGTH = 10;
-        private const int RETURN_VALUE_POSITION = 11;
-        private const byte RETURN_PACKET_HEADER = 0xD0;
-        private const byte DATA_LENGTH_POSITION = 7;
-
         internal Melsec3EProtocol(string ip, ushort port)
-            : base(ip, port, ERROR_CODE_POSITION, MIN_RESPONSE_LENGTH, RETURN_VALUE_POSITION, RETURN_PACKET_HEADER, DATA_LENGTH_POSITION)
+            : base(ip, port,
+                  errorCodePosition: 9,
+                  minResponseLength: 10,
+                  returnValuePosition: 11,
+                  returnPacketHeader: 0xD0,
+                  dataLengthPosition: 7)
         {
             base.PacketHead = new byte[] { 0x50, 0x00 };
         }
